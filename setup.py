856554692b9d2635f30e816f9112538780027b3e
Nandhagopal, K <89440790+NandhaxD@users.noreply.github.com>
2024-10-10 18:08:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nandha_helper',  # Replace with your actual package name
-    version='0.1.0',
+    name="nandha_helper",           # Your package name
+    version="0.1.0",                # Initial version
+    author="Nandhagopal K",         # Your name
+    author_email="nandhaxd@gmail.com",
+    description="A simple helper package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nandhaxd/nandha_helper",  # Your GitHub repo
     packages=find_packages(),
-    description='A simple package with one class',
-    long_description=open('README.md').read(), 
-    long_description_content_type='text/markdown', 
-    author='Nandhagopal K',
-    author_email='nandhaxd@gmail.com',
-    url='https://github.com/nandhaxd/nandha_helper',  # Replace with your project URL
-    license='MIT',
-    install_requires=[],  # List any dependencies your package needs
+    package_data={
+        'nandha_helper': ['data/User-Agent.txt'],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',  # Specify the minimum Python version
-)  
+    python_requires='>=3.6',
+)
